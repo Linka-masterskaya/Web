f33529d475ee67d0eb97259e39057d80814ff568
Egor Shkiria <zuhijan@yandex.ru>
2026-06-11 23:12:06 +0600

    feat: перенес логотип в паблик

--- src/shared/ui/logo/logo.tsx
+++ src/shared/ui/logo/logo.tsx
@@ -1,8 +1,7 @@
 import clsx from 'clsx'
-import logoPng from '../assets/logo.png'
 import styles from './logo.module.scss'
 import type { TLogoProps } from './types'
 
 export const Logo: React.FC<TLogoProps> = ({ alt = 'Logo', className, ...otherProps }) => (
-  <img src={logoPng} alt={alt} className={clsx(styles.logo, className)} {...otherProps} />
+  <img src="/logo.png" alt={alt} className={clsx(styles.logo, className)} {...otherProps} />
 )
