6c3c6e3e980f8f9f389182c16853c1eb69b0f485
Iana Rusachenko <iana2806@yandex.ru>
2026-06-15 18:45:10 +0300

    feat: improve login form

--- src/features/login/index.ts
+++ src/features/login/index.ts
@@ -1,2 +1,1 @@
-export { LoginButton } from './login-button'
 export { LoginForm } from './ui/login-form'
--- src/features/login/ui/login-form.tsx
+++ src/features/login/ui/login-form.tsx
@@ -1,6 +1,7 @@
 import { loginFormDefaultValues, loginFormSchema, type TLoginFormValues } from '@entities/auth'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Anchor, Button, PasswordInput, Stack, TextInput } from '@mantine/core'
+import { createUrl, routerPath } from '@shared/lib/routes'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
 
@@ -22,24 +23,32 @@ export const LoginForm = ({ onSubmit }: TLoginFormProps) => {
   } = form
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form noValidate onSubmit={handleSubmit(onSubmit)}>
       <Stack gap="md">
-        <TextInput placeholder="Email" {...register('email')} error={errors.email?.message} />
+        <TextInput
+          type="email"
+          autoComplete="email"
+          placeholder="Email"
+          {...register('email')}
+          error={errors.email?.message}
+        />
 
         <PasswordInput
+          autoComplete="current-password"
           placeholder="Пароль"
           {...register('password')}
           error={errors.password?.message}
         />
 
-        <Anchor component={Link} to="/auth/forgot-password">
+        <Anchor component={Link} to={createUrl(routerPath.authForgotPassword)}>
           Забыли пароль?
         </Anchor>
 
         <Button type="submit" loading={isSubmitting} variant="filled">
           Войти
         </Button>
-        <Button component={Link} to="/register" variant="outline">
+
+        <Button component={Link} to={createUrl(routerPath.authRegister)} variant="outline">
           Зарегистрироваться
         </Button>
       </Stack>
