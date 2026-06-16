2105e02eac6c75a3c9366ecf4d479f3f961ccba7
Sofia Lesnikova <sooofiawork@gmail.ru>
2026-06-16 23:08:47 +0300

    feat: add close button and styles

--- src/pages/forgot-password-page/forgot-password.page.tsx
+++ src/pages/forgot-password-page/forgot-password.page.tsx
@@ -1,10 +1,14 @@
 import type { TForgotPasswordFormValues } from '@entities/auth'
 
 import { ForgotPasswordForm } from '@features/forgot-password'
-import { Flex, Text, Title } from '@mantine/core'
+import { CloseButton, Flex, Text, Title } from '@mantine/core'
 import { useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router'
+
+import styles from './forgot-password.page.module.scss'
 
 export const ForgotPasswordPage: React.FC = () => {
+  const navigate = useNavigate()
   // TODO: здесь должны быть какая-то логика для отправки запроса на смену пароля,
   // но пока что тут имитация, чтобы кнопка делала вид, что запрос идет
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)
@@ -29,14 +33,39 @@ export const ForgotPasswordPage: React.FC = () => {
       }, 2000)
     })
   }
-  return (
-    <Flex direction="column" align="center" justify="center" w="100%" h="100%" gap="20px">
-      <Title order={1}>Забыли пароль?</Title>
-      <Text>
-        Введите адрес электронной почты, которую вы использовали для регистрации в сервисе
-      </Text>
 
-      <ForgotPasswordForm onSubmit={handleForgotPasswordSubmit} />
+  const handleGoToLogin = () => {
+    navigate('/auth')
+  }
+
+  return (
+    <Flex
+      component="section"
+      direction="column"
+      align="center"
+      justify="center"
+      w="100%"
+      mih="100dvh"
+      className={styles.content}
+    >
+      <CloseButton
+        aria-label="Вернуться на страницу входа"
+        size="lg"
+        onClick={handleGoToLogin}
+        className={styles.closeButton}
+      />
+
+      <Flex direction="column" align="center" gap="20px" className={styles.contentInner}>
+        <Title order={1} ta="center">
+          Забыли пароль?
+        </Title>
+
+        <Text size="sm" className={styles.text}>
+          Введите адрес электронной почты, которую вы использовали для регистрации в сервисе
+        </Text>
+
+        <ForgotPasswordForm onSubmit={handleForgotPasswordSubmit} />
+      </Flex>
     </Flex>
   )
 }
