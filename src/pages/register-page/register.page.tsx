2c3acf0597503b2ee6112540335d218b7666fdce
romanov-km <workrkm@mail.ru>
2026-06-20 11:43:56 +0300

    feat/#39-add-register-page

--- src/pages/register-page/register.page.tsx
+++ src/pages/register-page/register.page.tsx
@@ -1,17 +1,27 @@
-import type { TRegisterFormValues } from '@entities/auth'
+import { type TRegisterFormValues, useAuthStore } from '@entities/auth'
 import { RegisterForm } from '@features/register'
 import { Flex, Title } from '@mantine/core'
+import { createUrl, routerPath } from '@shared/lib/routes'
+import { useNavigate } from 'react-router'
+import styles from './register.page.module.scss'
 
 export const RegisterPage = () => {
-  const handleSubmit = async (values: TRegisterFormValues) => {
+  const navigate = useNavigate()
+  const login = useAuthStore((state) => state.login)
+
+  const handleSubmit = async (_values: TRegisterFormValues) => {
     // biome-ignore lint/suspicious/noConsole: debug only
-    console.log('Данные регистрации:', values)
+    console.log('Данные регистрации:', _values)
     // TODO: интеграция с API
+    await login()
+    navigate(createUrl(routerPath.dashboard))
   }
 
   return (
-    <Flex direction="column" align="center" gap="md">
-      <Title order={1}>Регистрация</Title>
+    <Flex className={styles.content}>
+      <Title order={1} className={styles.title}>
+        Регистрация
+      </Title>
       <RegisterForm onSubmit={handleSubmit} />
     </Flex>
   )
