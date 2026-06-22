4371dc74bde71d5dcba41b7bbe273d1000e96864
Iana Rusachenko <iana2806@yandex.ru>
2026-06-22 17:13:48 +0300

    feat: forgot password page with form and success state

--- src/pages/forgot-password-page/forgot-password.page.tsx
+++ src/pages/forgot-password-page/forgot-password.page.tsx
@@ -1,14 +1,20 @@
-import { ForgotPassword } from '@features/forgot-password'
+import { ForgotPasswordForm, type TForgotPasswordFormValues } from '@entities/auth'
 import { CloseButton, Flex, Text, Title } from '@mantine/core'
+import { createUrl, routerPath } from '@shared/lib/routes'
+import { useState } from 'react'
 import { useNavigate } from 'react-router'
-
 import styles from './forgot-password.page.module.scss'
 
 export const ForgotPasswordPage: React.FC = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false)
   const navigate = useNavigate()
 
   const handleGoToLogin = () => {
-    navigate('/auth')
+    navigate(createUrl(routerPath.auth))
+  }
+
+  const handleSubmit = async (_values: TForgotPasswordFormValues) => {
+    setIsSubmitted(true) // форма пока что никуда не отправляется
   }
 
   return (
@@ -28,16 +34,24 @@ export const ForgotPasswordPage: React.FC = () => {
         className={styles.closeButton}
       />
 
-      <Flex direction="column" align="center" gap="20px" className={styles.contentInner}>
+      <Flex direction="column" gap="20px" className={styles.contentInner}>
         <Title order={1} ta="center">
           Забыли пароль?
         </Title>
 
-        <Text size="sm" className={styles.text}>
-          Введите адрес электронной почты, которую вы использовали для регистрации в сервисе
-        </Text>
+        {isSubmitted ? (
+          <Text size="sm" className={styles.successText}>
+            Ваш запрос на восстановление пароля принят. Инструкция отправлена на Вашу почту.
+          </Text>
+        ) : (
+          <>
+            <Text size="sm" className={styles.text}>
+              Введите адрес электронной почты, которую вы использовали для регистрации в сервисе
+            </Text>
 
-        <ForgotPassword />
+            <ForgotPasswordForm onSubmit={handleSubmit} />
+          </>
+        )}
       </Flex>
     </Flex>
   )
