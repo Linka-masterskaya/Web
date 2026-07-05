c6600641d841a047101e1d63abfdbb268d061d84
romanov-km <workrkm@mail.ru>
2026-07-05 18:13:02 +0300

    fix/#48-one-source-of-truth

--- src/features/filter-favorite/config.ts
+++ src/features/filter-favorite/config.ts
@@ -1,2 +1,4 @@
-export const FILTER_FAVORITE_QUERY_PARAM = 'favorite'
+import { routeQueryParams } from '@shared/lib/routes'
+
+export const FILTER_FAVORITE_QUERY_PARAM = routeQueryParams.favorite
 export const FILTER_FAVORITE_ACTIVE_VALUE = 'true'
