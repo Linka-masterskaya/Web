5c43adcff1e66dc34908f846851958d319c87fbe
Iana Rusachenko <iana2806@yandex.ru>
2026-09-25 13:49:29 +0300

    feat: add pack cover

--- src/features/section-contents-browser/ui/section-contents-browser.tsx
+++ src/features/section-contents-browser/ui/section-contents-browser.tsx
@@ -9,6 +9,7 @@ import { useOpenPublishSet } from '@features/publish-set'
 import { RenameFolderModal } from '@features/rename-folder'
 import { RenameSetModal } from '@features/rename-set'
 import { SendSet } from '@features/set'
+import { CoverPickerModal } from '@features/set-settings'
 import { Button, Group, Loader, Stack, Text } from '@mantine/core'
 import { getApiErrorMessage } from '@shared/lib/api'
 import { useModal } from '@shared/lib/modal'
@@ -155,6 +156,16 @@ export const SectionContentsBrowser: FC<TSectionContentsBrowserProps> = ({
     })
   }
 
+  const handleChangePackCover = (pack: TPackContentItem) => {
+    open({
+      size: 1280,
+      padding: 0,
+      radius: 20,
+      withCloseButton: false,
+      content: <CoverPickerModal packId={pack.id} />,
+    })
+  }
+
   const handleRenameFolder = (folder: TFolderContentItem) => {
     open({
       size: 518,
@@ -288,6 +299,11 @@ export const SectionContentsBrowser: FC<TSectionContentsBrowserProps> = ({
       id: 'rename',
       label: 'Переименовать',
       onClick: handleRenamePack,
+    },
+    {
+      id: 'change-cover',
+      label: 'Изменить обложку',
+      onClick: handleChangePackCover,
     },
     {
       id: 'duplicate',
--- src/features/set-settings/index.ts
+++ src/features/set-settings/index.ts
@@ -6,5 +6,6 @@ export {
   SET_SETTINGS_DEFAULT_VALUES,
 } from './config'
 export type { TSetSettings } from './model/set-settings.schema'
+export { CoverPickerModal } from './ui/cover-picker-modal'
 export { SetSettings } from './ui/set-settings'
 export { useOpenSetSettings } from './use-open-set-settings'
